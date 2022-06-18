59ef8bd9c03358c9f0a461039ade61b2e7dc750c
Charles Reis Ribeiro <charles.r.ribeiro@gmail.com>
2022-06-18 16:24:01 -0300

    Create global championInfo

--- index.js
+++ index.js
@@ -5,8 +5,26 @@ const request = require('request');
 
 const lol_url = "http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json";
 
-function processSingleChampionData(champion){
-  console.log(champion);
+let championInfo = [];
+
+function processSingleChampionData(champion) {
+  const blurb = champion.blurb;
+  const id = champion.id;
+  const { attack, defense, difficulty, magic } = champion.info;
+  const name = champion.name;
+  const partype = champion.partype;
+  const tags = champion.tags;
+  const key = champion.key;
+
+  championInfo.push({
+    blurb, id, attack, defense, difficulty, magic, name, partype, tags
+  });
+
+  console.log(key);
+
+  if(key == 143){
+    console.log("foi...");
+  }
 }
 
 request.get({
@@ -23,13 +41,6 @@ request.get({
 
     for (var attributename in data) {
       // console.log(attributename + ": " + data[attributename].title);
-
-      // const blurb = data[attributename].blurb;
-      // const id = data[attributename].id;
-      // const { attack, defense, difficulty, magic } = data[attributename].info;
-      // const name = data[attributename].name;
-      // const partype = data[attributename].partype;
-
       processSingleChampionData(data[attributename]);
     }
   }
