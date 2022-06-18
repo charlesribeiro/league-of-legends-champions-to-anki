18a2920244b2324d3dccd73a7cd4c01100bc0120
Charles Reis Ribeiro <charles.r.ribeiro@gmail.com>
2022-06-18 17:09:56 -0300

    Add image column to csv

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-// const iconv = require("iconv-lite");
-// const fs = require('fs');
 const request = require('request');
 
-const lol_url = "http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json";
+const base_lol_url = "http://ddragon.leagueoflegends.com/cdn/12.11.1/"
+const lol_url = base_lol_url + "data/en_US/champion.json";
+const image_url = "img/champion/";
 
 let championInfo = [];
+
+function writeChampionInfoToCSV() {
+  const csvWriter = createCsvWriter({
+    path: process.cwd() + "/results/lolChampions.csv",
+    header: [
+      { id: 'name', title: 'NAME' },
+      { id: 'blurb', title: 'BLURB' },
+      { id: 'id', title: 'ID' },
+      { id: 'attack', title: 'ATTACK' },
+      { id: 'defense', title: 'DEFENSE' },
+      { id: 'difficulty', title: 'DIFFICULTY' },
+      { id: 'magic', title: 'MAGIC' },
+      { id: 'partype', title: 'PARTYPE' },
+      { id: 'tags', title: 'TAGS' },
+      { id: 'image', title: 'IMAGE' },
+      { id: 'key', title: 'KEY' },
+    ]
+  });
+
+  csvWriter.writeRecords(championInfo)
+    .then(() => {
+      console.log('...Done');
+    });
+}
 
 function processSingleChampionData(champion) {
   const blurb = champion.blurb;
@@ -15,15 +39,16 @@ function processSingleChampionData(champion) {
   const partype = champion.partype;
   const tags = champion.tags;
   const key = champion.key;
+  const image = base_lol_url + image_url + champion.image.full;
 
   championInfo.push({
-    blurb, id, attack, defense, difficulty, magic, name, partype, tags
+    name, blurb, id, attack, defense, difficulty, magic, partype, tags, image, key
   });
 
   console.log(key);
 
-  if(key == 143){
-    console.log("foi...");
+  if (key == 143) {
+    writeChampionInfoToCSV();
   }
 }
 
@@ -37,7 +62,7 @@ request.get({
   } else if (res.statusCode !== 200) {
     console.log('Status:', res.statusCode);
   } else {
-    // console.log(data);
+    console.log(data);
 
     for (var attributename in data) {
       // console.log(attributename + ": " + data[attributename].title);
