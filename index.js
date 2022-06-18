8203ac12299e95a40d3091b5b1d5e8bf9eae779e
Charles Reis Ribeiro <charles.r.ribeiro@gmail.com>
2022-06-18 15:46:17 -0300

    Create processSingleChampionData function

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-// const fetch = require('node-fetch');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+// const iconv = require("iconv-lite");
+// const fs = require('fs');
+const request = require('request');
 
-let lol_url = "http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json";
+const lol_url = "http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json";
 
-
-'use strict';
-var request = require('request');
-
-// var url = 'https://api.github.com/users/rsp';
+function processSingleChampionData(champion){
+  console.log(champion);
+}
 
 request.get({
-    url: lol_url,
-    json: true,
-    headers: {'User-Agent': 'request'}
-  }, (err, res, {data}) => {
-    if (err) {
-      console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-      console.log('Status:', res.statusCode);
-    } else {
-      console.log(data);
-    //   data.array.forEach(element => {
-    //     forEach
-    //   });(champion => {
-    //     console.log(champion);
-    //   });
+  url: lol_url,
+  json: true,
+  headers: { 'User-Agent': 'request' }
+}, (err, res, { data }) => {
+  if (err) {
+    console.log('Error:', err);
+  } else if (res.statusCode !== 200) {
+    console.log('Status:', res.statusCode);
+  } else {
+    // console.log(data);
+
+    for (var attributename in data) {
+      // console.log(attributename + ": " + data[attributename].title);
+
+      // const blurb = data[attributename].blurb;
+      // const id = data[attributename].id;
+      // const { attack, defense, difficulty, magic } = data[attributename].info;
+      // const name = data[attributename].name;
+      // const partype = data[attributename].partype;
+
+      processSingleChampionData(data[attributename]);
     }
+  }
 });
